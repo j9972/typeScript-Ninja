dd3ce466e3da621df4767943225d56351c6aeee4
jung su young <j9972@naver.com>
2022-05-26 15:21:07 +0900

    any

--- sandbox.ts
+++ sandbox.ts
@@ -1,38 +1,15 @@
-//explicit types -> 타입을 정했기때문에 다른 타입은 무시된다.
-let character : string;
-let age : number;
-let check : boolean;
+let age : any = 25;
 
-// array
-let ninjas: string[]; // empty array에 push는 error
-// let ninjas: string[] = [];  이렇게 하면 위의 에러 해결
+age = true;
+age = 'hello'
+age = {name: 'mario', age: 24 }
 
-// ninjas.push('shau') 가능
-
-ninjas = ['yo', 'si']
-
-// union types
-// ex - array
-let mixed : (string|number|boolean)[] = [];
-mixed.push('hello');
-mixed.push(20);
+// type 바꾸고 싶으면
+let mixed : any[] = [];
+mixed.push(5);
+mixed.push('mario');
 mixed.push(false);
 
-// ex - normal var
-let uid : string|number|boolean;
-uid = '123';
-uid = 123;
-uid = false;
-
-// objects -> 2가지 ways
-let ninjaOne : object;
-ninjaOne = { name: 'yoshi', age: 24};
-
-// 다른 방법이지만, new property를 넣으면 에러남
-let ninjaTwo : {
-    name: string,
-    age: number,
-    beltColor: string
-}
-
-ninjaTwo = { name: 'mario', age: 23, beltColor: 'black'}
+let ninja : {name: any, age: any}
+ninja = {name: 'nasd', age : 23}
+ninja = {name: 23, age : '23'}
