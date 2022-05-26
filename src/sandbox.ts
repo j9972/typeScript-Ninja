79b8f518dc227a9cfec765f808b07a0a895db50f
jung su young <j9972@naver.com>
2022-05-26 16:23:36 +0900

    func parameter TYPE

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,26 +1,22 @@
-let greet : Function;
+// way1)
 
-greet = () => {
-    console.log('greet');    
+const logDetails = (uid: string|number, item:string) => {
+    console.log(`${item} has a uid of ${uid}`);
 }
 
-// ?는 option mark이므로 c 에 대한 값이 없어도 된다 -> 없으면 undefined
-// void를 써주면, undefined되면 전달이 되지 않게끔 해준다
-const add = (a:number, b:number, c?:number|string): void => {
-    console.log(a+b);
-    console.log(c);
-    
+// parameter로 객체가 들어올 수 있다.
+const greet = (user: {name: string, uid: string|number}) => {
+    console.log(`${user.name}say hello`);
 }
 
-// ?는 없으면 default 값 주기 
-const put = (a:number, b:number, c:number|string = 10) => {
-    console.log(a+b);
+// way2)
+type StringOrNum = string | number
+type objWithName = {name:string, uid: StringOrNum}
+
+const logDetailsB = (uid: StringOrNum, item:string) => {
+    console.log(`${item} has a uid of ${uid}`);
 }
 
-add(5,10)
-
-const minus = (a:number, b:number) => {
-    return a-b;
+const greetB = (user: objWithName) => {
+    console.log(`${user.name}say hello`);
 }
-let result = minus(10,7);
-// result = '' 이렇게 변경이 불가능, return으로 끝내 버렸기 때문
