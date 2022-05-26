864a36f5beab77de0da58fb0679e91dc6aaff603
jung su young <j9972@naver.com>
2022-05-26 14:57:15 +0900

    learn obj and array

--- sandbox.ts
+++ sandbox.ts
@@ -1,13 +1,32 @@
-let character = 'mario'
-let age = 24;
-let check = false
+// array
+let names = ['c', 'a','r', 'R','o', 't']
 
-character = 'jung'
-age = 40;
-check = true;
+names.push('thoad');
 
-const circ = (diameter: number) => {
-    return diameter * Math.PI
+
+let nums = [10,20,30,40]
+
+nums.push(25);
+
+let mixed = ['ken',2,'chun', 3, 5];
+
+mixed.push('ryt');
+mixed.push(10);
+
+// object
+let ninja = {
+    name : 'mario',
+    belt : 'black',
+    age: 24
 }
 
-console.log(circ(3));
+ninja.age = 40;
+ninja.name = 'gyu';
+//ninja.skills = ['fighting','sleeping'] -> 없는 property라서 안됨
+
+ninja = {
+    name : 'mario',
+    belt : 'black',
+    age: 24,
+    // skills: [] 이렇게 쓰는거 안됨 
+}
