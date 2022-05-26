e1bc31e8261b84426a2cff586f52d3c75211a9ee
jung su young <j9972@naver.com>
2022-05-26 22:30:46 +0900

    imterface with class

--- src/app.ts
+++ src/app.ts
@@ -1,65 +1,34 @@
-//interface
-interface IsPerson {
-    name: string;
-    age: number;
-    speak(a: string): void;
-    spend(a:number): number;
-}
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
 
-// interface에[ 없는 속성을 넣으면 에러가 난다.]
-const me : IsPerson = {
-    name: 'shaun',
-    age: 30,
-    speak(text: string): void{
-        console.log(text);
-    },
-    spend(amount: number) : number {
-        console.log('I spend', amount);
-        return amount;
-    }
-};
+// let docOne: HasFormatter; -> 일반 변수
+// let docTwo: HasFormatter;
 
-const greetPerson = (person: IsPerson) => {
-    console.log('hello', person.name);
-    
-}
+// docOne = new Invoice('yoshi', 'web work', 250);
+// docTwo = new Payment('mario', 'plumbing', 200);
 
-greetPerson(me)
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
 
-//classes
-import {Invoice} from './classes/Invoice.js'
-
-const invOne = new Invoice('mario', 'work on the mario web', 250);
-const invTwo = new Invoice('luo', 'work on the luo web', 230);
-
-let invoices: Invoice[] = []; // array
-invoices.push(invOne);
-invoices.push(invTwo);
-console.log(invoices);
-
-invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());  
-})
-
-
-// 표현을 이렇게도 할 수 있음 - Form
-//const form = document.querySelector('form')!;
-const form = document.querySelector('.new-item-form') as HTMLFormElement; // form class name
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
 
 // inputs
-const type = document.querySelector('#type') as HTMLSelectElement;
-const toform = document.querySelector('#toform') as HTMLInputElement;
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
 form.addEventListener('submit', (e: Event) => {
-    e.preventDefault();
-    console.log(
-        type.value,
-        toform.value,
-        details.value,
-        amount.valueAsNumber,
-        // value -> string, valueAsNumber -> number
-    );
-    
-})
+  e.preventDefault();
+
+  let doc: HasFormatter;
+  if (type.value === 'invoice') {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+  console.log(doc);
+});
